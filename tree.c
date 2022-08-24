cea503ef3f0a84f2a9201fa486f94ce4cb58a04b
Ajil <94738733+ajil02@users.noreply.github.com>
2022-08-24 13:39:43 +0530

    Add files via upload

---
+++ tree.c
@@ -0,0 +1,76 @@
+/*Program to perform pre-order, in-order, post-order traversals on binary trees*/
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	struct node *left;
+	int data;
+	struct node *right;
+};
+
+struct node * newnode(int val)
+{
+	struct node * new=(struct node *)malloc(sizeof(struct node));
+	new->data=val;
+	new->right=NULL;
+	new->left=NULL;
+	return(new);
+}
+
+void preorder(struct node * root)
+{
+	if(root==NULL)
+	{
+		return;
+	}
+	printf("%d\t",root->data);
+	preorder(root->left);
+	preorder(root->right);
+}
+
+void inorder(struct node * root)
+{
+	if(root==NULL)
+	{
+		return;
+	}
+	inorder(root->left);
+	printf("%d\t",root->data);
+	inorder(root->right);
+}
+
+void postorder(struct node * root)
+{
+	if(root==NULL)
+	{
+		return;
+	}
+	postorder(root->left);
+	postorder(root->right);
+	printf("%d\t",root->data);
+}
+
+void main()
+{
+	struct node * root;
+	root=newnode(1);
+	root->left=newnode(2);
+	root->right=newnode(3);
+	root->left->left=newnode(4);
+	root->left->right=newnode(5);
+	root->right->left=newnode(6);
+	root->right->right=newnode(7);
+	
+	printf("Pre-order traversal is:\n");
+	preorder(root);
+	printf("\n");
+	
+	printf("In-order traversal is:\n");
+	inorder(root);
+	printf("\n");
+	
+	printf("Post-order traversal is:\n");
+	postorder(root);
+	printf("\n");
+}
